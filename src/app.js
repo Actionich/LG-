1cd37a78ef29b75c5fe31a69acc8ce226a22a677
Actionich <actionich@gmail.com>
2025-05-04 21:30:11 +0900

    서비스 소개 React 랜딩페이지 빌드 오류 수정 및 lucide-react 추가

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // 앱의 루트 컴포넌트 역할을 하는 app.js 파일입니다.
 // LGLifecareServiceSite를 import하여 내보냅니다.
 import React from 'react';
-import LGLifecareServiceSite from './LGLifecareServiceSite';
+import LGLifecareServiceSite from './LGLifecareServiceSite.jsx';
 
 // App 컴포넌트: 전체 서비스 사이트를 렌더링합니다.
 function App() {
