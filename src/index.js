81576d9eeb6991b0ebf0b0d64964c5838df7067c
Actionich <actionich@gmail.com>
2025-05-04 18:00:52 +0900

    feat: app.js 파일 생성 및 루트 컴포넌트 구성

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// React 앱의 진입점 역할을 하는 index.js 파일입니다.
+// App 컴포넌트를 불러와서 브라우저에 렌더링합니다.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+// React 18 이상에서는 createRoot를 사용합니다.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); 
